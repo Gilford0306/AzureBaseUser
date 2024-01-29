a0cab912254b2900d09cd81cb9dfac7db988f910
Gilford0306 <gilford0306@gmail.com>
2024-01-29 16:05:39 +0200

    Add project files.

---
+++ AzureBaseUser/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using AzureBaseUser.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+
+namespace AzureBaseUser.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private UserDBContext userDBContext;
+
+        public UserController(UserDBContext _userDBContext)
+        {
+            this.userDBContext = _userDBContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+
+
+            return Ok(this.userDBContext.Users.ToList());
+        }
+        [HttpPost]
+        public async Task<ActionResult<User>> Add(User user)
+        {
+
+
+
+            this.userDBContext.Users.Add(user);
+            this.userDBContext.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<ActionResult<User>> Delete(User user)
+        {
+
+
+
+            this.userDBContext.Users.Remove(user);
+            this.userDBContext.SaveChanges();
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<ActionResult<User>> Update(User user)
+        {
+
+            this.userDBContext.Users.Update(user);
+            this.userDBContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ AzureBaseUser/Model/UserDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace AzureBaseUser.Model
+{
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext()
+        {
+        }
+
+        public UserDBContext(DbContextOptions<UserDBContext> options)
+        : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+
+
+    }
+}
